4440b22e0940a805c349f8d2f002d3b4f934e331
Eric <hoang.e.nguyen@gmail.com>
2015-09-24 19:58:02 -0500

    Fix recipe check box on dashboard

--- client/app/dashboard/dashboard-controller.js
+++ client/app/dashboard/dashboard-controller.js
@@ -1,9 +1,17 @@
 angular.module('wtf.dashboard', [])
-  .controller('DashboardController', function($scope, $window, Auth, Recipe) {
+  .controller('DashboardController', function($scope, $window) {
+    //assuming a getrequest was done and data is now available
+    
+    $scope.allRecipes = [
+        {id: 1, title: "Mom's Lasagna", ingredients: ["tomatoes", "ricotta cheese", "italian sausage"]},
+        {id: 2, title: "Mac&Cheese", ingredients: ["macaroni", "cheese", "milk", "salt", "pepper"]},
+        {id: 3, title: "Chocolate chip cookies", ingredients: ["chocolate", "flour", "salt", "pepper"]}
+    ];
 
     $scope.deleteRecipe = function(){};
     $scope.goShopping = function(){};
-    $scope.getAllRecipes = function(){}
+    $scope.getAllRecipes = function(){
+    }
 
     $scope.getAllRecipes();
   });
